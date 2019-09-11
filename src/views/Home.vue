592b8ba01904057540f30ce7a37f39f5b3eb8a5d
qiutian00 <1476102147@qq.com>
2019-09-11 12:55:14 +0800

    feat: init the project

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <button @click="getInfo" :style="{ background: bgColor }">请求数据</button>
+    <!-- <img alt="Vue logo" src="../assets/img/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import { getUserInfo } from '@/api/user'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      url: '',
+      bgColor: ''
+    }
+  },
+  methods: {
+    getInfo () {
+      getUserInfo({ userId: 21 }).then(res => {
+        console.log('res: ', res)
+      })
+    },
+  }
+}
+</script>
